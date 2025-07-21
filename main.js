59f677939d870d9eed3b99670b26e18210cbadd6
yamila_rengifo <yamirngifo18@gmail.com>
2025-07-21 17:51:33 -0300

    listo

--- main.js
+++ main.js
@@ -24,11 +24,37 @@ const productos = [
   { nombre: "Cinta métrica", categoria: "Ferretería", precio: 25 },
 ];
 
+const imagenesProductos = {
+  "Laptop Lenovo": "imagenes/laptop_lenovo.png",
+  "Celular Samsung": "imagenes/celular_samsung.png",
+  "Auriculares JBL": "imagenes/auriculares_jbl.png",
+  "Mouse Logitech": "imagenes/mouse_logitech.png",
+  "Teclado Redragon": "imagenes/teclado_reddragon.png",
+
+  "Sofá 3 plazas": "imagenes/sofa3plazas.png",
+  "Mesa de comedor": "imagenes/mesa_de_comedor.png",
+  "Lámpara de pie": "imagenes/lampara_de_pie.png",
+  "Alfombra persa": "imagenes/alfobra_persa.png",
+  "Silla ergonómica": "imagenes/silla_ergonomica.png",
+
+  "Remera blanca": "imagenes/remera_blanca.png",
+  "Pantalón jeans": "imagenes/pantalones_jeans.png",
+  "Campera de cuero": "imagenes/campera_de_cuero.png",
+  "Zapatillas Nike": "imagenes/zapatillas_nike.png",
+  "Gorra negra": "imagenes/gorra_negra.png",
+
+  "Martillo": "imagenes/martillo.png",
+  "Taladro eléctrico": "imagenes/taladro_electrico.png",
+  "Caja de herramientas": "imagenes/caja_de_herramientas.png",
+  "Destornillador": "imagenes/destornillador.png",
+  "Cinta métrica": "imagenes/cinta_metrica.png"
+};
+
 let contenedor2 = document.querySelector("#catalogo");
 
 for (let i = 0; i < productos.length; i++) {
-  if (productos[i].categoria === "Ferretería") {
-    let imagenURL = imagenes[productos[i].nombre] || "img/placeholder.jpg"; // imagen por defecto si no se encuentra
+  if (productos[i].categoria === "Hogar") {
+    let imagenURL = imagenesProductos[productos[i].nombre] || "img/placeholder.jpg";
 
     contenedor2.innerHTML += `
       <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
